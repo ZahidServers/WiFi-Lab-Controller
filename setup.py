9254a15f6074437d160c9924f939a5135456cf2c
Mohammed Zahid Wadiwale <63946803+ZahidServers@users.noreply.github.com>
2025-12-05 15:10:44 +0530

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wifilab",
+    version="1.0.0",
+    py_modules=["app"],
+    author="Mohammed Zahid Wadiwale",
+    author_email="support@webaon.com",
+    description="Wi-Fi Lab Controller: Safe WiFi testing toolkit with scanning, fake AP, DNS redirection.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZahidServers/WiFi-Lab-Controller",
+    license="GPL-3.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Security",
+        "Topic :: System :: Networking",
+    ],
+    install_requires=[],
+    python_requires=">=3.9",
+    entry_points={
+        "console_scripts": [
+            "wifilab=app:main"
+        ]
+    },
+)
