12e1eeabd4a4ba703f7308f454befed4dfa81d94
Mohammed Zahid Wadiwale <63946803+ZahidServers@users.noreply.github.com>
2025-12-11 13:17:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wifilab",
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     author="Mohammed Zahid Wadiwale",
     author_email="support@webaon.com",
